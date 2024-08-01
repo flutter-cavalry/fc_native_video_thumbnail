51c1122e57928636a971009ddaf55e2b0ddfcc62
Mgen <m@mgenware.com>
2024-08-02 02:56:47 +0800

    Update android project

--- example/android/app/src/main/kotlin/com/fluttercavalry/fc_native_video_thumbnail_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/fluttercavalry/fc_native_video_thumbnail_example/MainActivity.kt
@@ -2,5 +2,4 @@ package com.fluttercavalry.fc_native_video_thumbnail_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
