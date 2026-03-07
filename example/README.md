9fb406f5060815403f29aac0e0854a1c70ed9930
Mgen <m@mgenware.com>
2026-03-07 16:42:40 +0800

    Regenerate example project

--- example/README.md
+++ example/README.md
@@ -1,6 +1,17 @@
-# fc_native_image_resize_example
+# fc_native_video_thumbnail_example
 
-To run this example project locally.
+Demonstrates how to use the fc_native_video_thumbnail plugin.
 
-- cd `example`
-- `flutter run -d <your device>`
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Learn Flutter](https://docs.flutter.dev/get-started/learn-flutter)
+- [Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Flutter learning resources](https://docs.flutter.dev/reference/learning-resources)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
--- example/macos/RunnerTests/RunnerTests.swift
+++ example/macos/RunnerTests/RunnerTests.swift
@@ -2,11 +2,27 @@ import Cocoa
 import FlutterMacOS
 import XCTest
 
+
+@testable import fc_native_video_thumbnail
+
+// This demonstrates a simple unit test of the Swift portion of this plugin's implementation.
+//
+// See https://developer.apple.com/documentation/xctest for more information about using XCTest.
+
 class RunnerTests: XCTestCase {
 
-  func testExample() {
-    // If you add code to the Runner application, consider adding tests here.
-    // See https://developer.apple.com/documentation/xctest for more information about using XCTest.
+  func testGetPlatformVersion() {
+    let plugin = FcNativeVideoThumbnailPlugin()
+
+    let call = FlutterMethodCall(methodName: "getPlatformVersion", arguments: [])
+
+    let resultExpectation = expectation(description: "result block must be called.")
+    plugin.handle(call) { result in
+      XCTAssertEqual(result as! String,
+                     "macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
+      resultExpectation.fulfill()
+    }
+    waitForExpectations(timeout: 1)
   }
 
 }
